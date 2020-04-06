c92c901b128b4485fc9fe878120d5ea2d9c87a79
Godwin Olletey <godwinolle@gmail.com>
2020-04-05 21:05:57 -0400

    Added a high and low temperature

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ const weather = async (zip) =>{
     const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${apiKey}`);
     const data = await response.json();
     const temp = await data.main.temp;
+    const high = await data.main.temp_max;
+    const low = await data.main.temp_min;
     const currentWeather = await imperialTemp(temp);
+    const highWeather = await imperialTemp(high);
+    const lowWeather = await imperialTemp(low);
     const city = await data.name;
     const icon = await data.weather[0].icon;
     console.log(city);
@@ -18,10 +22,14 @@ const weather = async (zip) =>{
     const currentTempHTML = document.querySelector('.currentTemp');
     const weatherIcon = document.getElementById('weatherIcon');
     const location = document.querySelector('.located');
+    const highTempHTML = document.querySelector('.highTemp');
+    const lowTempHTML = document.querySelector('.lowTemp');
 
     currentTempHTML.innerHTML = `${currentWeather}`;
     weatherIcon.src = `icons/${icon}.png`;
     location.innerHTML = `${city}, US`;
+    highTempHTML.innerHTML = `${highWeather}`;
+    lowTempHTML.innerHTML = `${lowWeather}`;
 
 };
 
@@ -29,8 +37,7 @@ const cityForm = document.querySelector('form');
 
 cityForm.addEventListener('submit', e=>{
     e.preventDefault();
-
     const cityZIP = cityForm.zip.value.trim();
     console.log(cityZIP);
     weather(cityZIP);
-})
+});

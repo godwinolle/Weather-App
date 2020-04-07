74edd242bca8b1956de04dccc068402d95f77d8f
Godwin Olletey <godwinolle@gmail.com>
2020-04-07 02:55:22 -0400

    Added try catch

--- app.js
+++ app.js
@@ -6,33 +6,38 @@ function imperialTemp(temp){
 }
 
 const weather = async (zip) =>{
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${apiKey}`);
-    const data = await response.json();
-    const temp = await data.main.temp;
-    const high = await data.main.temp_max;
-    const low = await data.main.temp_min;
-    const currentWeather = await imperialTemp(temp);
-    const highWeather = await imperialTemp(high);
-    const lowWeather = await imperialTemp(low);
-    const city = await data.name;
-    const icon = await data.weather[0].icon;
-    const description = await data.weather[0].description;
-    console.log(city);
-    console.log(currentWeather);
+    try{
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${apiKey}`);
+        const data = await response.json();
+        const temp = await data.main.temp;
+        const high = await data.main.temp_max;
+        const low = await data.main.temp_min;
+        const currentWeather = await imperialTemp(temp);
+        const highWeather = await imperialTemp(high);
+        const lowWeather = await imperialTemp(low);
+        const city = await data.name;
+        const icon = await data.weather[0].icon;
+        const description = await data.weather[0].description;
+        
 
-    const currentTempHTML = document.querySelector('.currentTemp');
-    const weatherIcon = document.getElementById('weatherIcon');
-    const descriptionHTML = document.getElementById('description');
-    const highTempHTML = document.querySelector('.highTemp');
-    const lowTempHTML = document.querySelector('.lowTemp');
-    const location = document.querySelector('.located');
+        const currentTempHTML = document.querySelector('.currentTemp');
+        const weatherIcon = document.getElementById('weatherIcon');
+        const descriptionHTML = document.getElementById('description');
+        const highTempHTML = document.querySelector('.highTemp');
+        const lowTempHTML = document.querySelector('.lowTemp');
+        const location = document.querySelector('.located');
 
-    currentTempHTML.innerHTML = `${currentWeather}`;
-    weatherIcon.src = `icons/${icon}.png`;
-    descriptionHTML.innerHTML = `${description}`;
-    highTempHTML.innerHTML = `${highWeather}`;
-    lowTempHTML.innerHTML = `${lowWeather}`;
-    location.innerHTML = `${city}, US`;
+        currentTempHTML.innerHTML = `${currentWeather}`;
+        weatherIcon.src = `icons/${icon}.png`;
+        descriptionHTML.innerHTML = `${description}`;
+        highTempHTML.innerHTML = `${highWeather}`;
+        lowTempHTML.innerHTML = `${lowWeather}`;
+        location.innerHTML = `${city}, US`;
+    } catch(Error){
+        const currentTempHTML = document.querySelector('.currentTemp');
+        currentTempHTML.innerHTML = `Weather not located`;
+        currentTempHTML.style.fontSize = '10';
+    }
 
 };
 
